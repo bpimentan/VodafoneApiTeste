3e279004dd9a55f7dae15ebaf154a7b617ae041d
bpimentan <nogueiracb@hotmail.com>
2014-05-27 15:02:38 +0100

    First Push

---
+++ js/libs/menuswipe.js
@@ -0,0 +1,14 @@
+$( document ).on( "pagecreate", "#swipe-menu", function() {
+    $( document ).on( "swipeleft swiperight", "#swipe-menu", function( e ) {
+        // We check if there is no open panel on the page because otherwise
+        // a swipe to close the left panel would also open the right panel (and v.v.).
+        // We do this by checking the data that the framework stores on the page element (panel: open).
+        if ( $( ".ui-page-active" ).jqmData( "panel" ) !== "open" ) {
+            if ( e.type === "swipeleft" ) {
+                $( "#right-panel" ).panel( "open" );
+            } else if ( e.type === "swiperight" ) {
+                $( "#left-panel" ).panel( "open" );
+            }
+        }
+    });
+});
